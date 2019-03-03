35d4d8dcdb49b350bc236b99fcbafe4fc8c97d39
David zhang <d.ydzdavid@yahoo.com>
2019-03-03 21:27:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,16 +40,13 @@ function viewCart() {
 }
 
 function total() {
-  let t = 0;
-
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
-    }
+  // write your code here
+  var totalCost = 0;
+  for(var i = 0; i < cart.length ; i++){
+    totalCost += cart[i].itemPrice;
   }
-  return t;
+  return totalCost;
 }
-
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
